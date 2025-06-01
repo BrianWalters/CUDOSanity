c41f09e466aef86b535d69b2f82db6d015a99ef2
Brian Walters <brian.walters@hey.com>
2025-05-31 23:38:07 -0500

    forgot to add schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,7 +1,11 @@
 import {gameSchema} from './documents/game'
 import {teamMemberSchema} from './documents/teamMember'
+import {awardSchema} from './documents/award'
+import {seasonSchema} from './documents/season'
 
 export const schemaTypes = [
   gameSchema,
-  teamMemberSchema
+  teamMemberSchema,
+  awardSchema,
+  seasonSchema
 ]
